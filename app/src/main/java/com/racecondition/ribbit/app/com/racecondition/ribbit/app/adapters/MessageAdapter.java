dbcc53defada210af565ae3c576dd48f7f40ed5b
TimB0 <tboland1@gmail.com>
2014-06-22 11:16:48 -0700

    - added formatting and dates for each message

--- app/src/main/java/com/racecondition/ribbit/app/com/racecondition/ribbit/app/adapters/MessageAdapter.java
+++ app/src/main/java/com/racecondition/ribbit/app/com/racecondition/ribbit/app/adapters/MessageAdapter.java
@@ -2,6 +2,7 @@ package com.racecondition.ribbit.app.com.racecondition.ribbit.app.adapters;
 
 
 import android.content.Context;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,7 @@ import com.parse.ParseObject;
 import com.racecondition.ribbit.app.R;
 import com.racecondition.ribbit.app.com.racecondition.ribbit.app.utils.ParseConstants;
 
+import java.util.Date;
 import java.util.List;
 
 public class MessageAdapter extends ArrayAdapter<ParseObject> {
@@ -36,6 +38,7 @@ public class MessageAdapter extends ArrayAdapter<ParseObject> {
             holder = new ViewHolder();
             holder.iconImageView = (ImageView) convertView.findViewById(R.id.messageIcon);
             holder.nameLabel = (TextView) convertView.findViewById(R.id.senderLabel);
+            holder.timeLabel = (TextView)convertView.findViewById(R.id.timeLabel);
             convertView.setTag(holder);
         }
         else {
@@ -44,6 +47,15 @@ public class MessageAdapter extends ArrayAdapter<ParseObject> {
         }
 
         ParseObject message = mMessages.get(position);
+
+        Date createdAt = message.getCreatedAt();
+        long now = new Date().getTime();
+        String convertedDate = DateUtils.getRelativeTimeSpanString(
+                createdAt.getTime(),
+                now,
+                DateUtils.SECOND_IN_MILLIS).toString();
+
+        holder.timeLabel.setText(convertedDate);
 
         if (message.getString(ParseConstants.KEY_FILE_TYPE).equals(ParseConstants.TYPE_IMAGE)) {
             holder.iconImageView.setImageResource(R.drawable.ic_picture);
@@ -59,6 +71,7 @@ public class MessageAdapter extends ArrayAdapter<ParseObject> {
     private static class ViewHolder {
         ImageView iconImageView;
         TextView nameLabel;
+        TextView timeLabel;
     }
 
     public void refill(List<ParseObject> messages) {
