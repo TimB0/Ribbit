65ac3e0751b97cc89d5c8621fb7649944ac5d88e
TimB0 <tboland1@gmail.com>
2014-06-28 20:40:01 -0700

    - Setup and Complete Gravatar Images for Friends

--- app/src/main/java/com/racecondition/ribbit/app/com/racecondition/ribbit/app/adapters/UserAdapter.java
+++ app/src/main/java/com/racecondition/ribbit/app/com/racecondition/ribbit/app/adapters/UserAdapter.java
@@ -6,10 +6,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.parse.ParseUser;
 import com.racecondition.ribbit.app.R;
+import com.racecondition.ribbit.app.com.racecondition.ribbit.app.utils.MD5Util;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -32,7 +35,7 @@ public class UserAdapter extends ArrayAdapter<ParseUser> {
             // create View
             convertView = LayoutInflater.from(mContext).inflate(R.layout.user_item, null);
             holder = new ViewHolder();
-            //holder.iconImageView = (ImageView) convertView.findViewById(R.id.messageIcon);
+            holder.userImageView = (ImageView) convertView.findViewById(R.id.userImageView);
             holder.nameLabel = (TextView) convertView.findViewById(R.id.nameLabel);
             convertView.setTag(holder);
         }
@@ -42,6 +45,20 @@ public class UserAdapter extends ArrayAdapter<ParseUser> {
         }
 
         ParseUser user = mUsers.get(position);
+        String email = user.getEmail().toLowerCase();
+
+        if (email.equals("")) {
+            holder.userImageView.setImageResource(R.drawable.avatar_empty);
+        }
+        else {
+            String hash = MD5Util.md5Hex(email);
+            String gravatarUrl = "http://www.gravatar.com/avatar/" + hash +
+                    "?s=204&d=404";
+            Picasso.with(mContext)
+                    .load(gravatarUrl)
+                    .placeholder(R.drawable.avatar_empty)
+                    .into(holder.userImageView);
+        }
 
    /*     if (user.getString(ParseConstants.KEY_FILE_TYPE).equals(ParseConstants.TYPE_IMAGE)) {
             holder.iconImageView.setImageResource(R.drawable.ic_picture);
@@ -55,7 +72,7 @@ public class UserAdapter extends ArrayAdapter<ParseUser> {
     }
 
     private static class ViewHolder {
-        //ImageView iconImageView;
+        ImageView userImageView;
         TextView nameLabel;
     }
 
