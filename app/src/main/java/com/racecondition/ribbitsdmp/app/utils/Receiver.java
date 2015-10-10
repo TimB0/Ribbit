8e243f4bd73ff75e9edaf79553f661d4ece03677
Timothy Boland <Timothy.Boland.contractor@hilton.com>
2015-10-10 11:35:02 -0700

    saving 5.1+Release latest before creating if-using... branch

---
+++ app/src/main/java/com/racecondition/ribbitsdmp/app/utils/Receiver.java
@@ -0,0 +1,21 @@
+package com.racecondition.ribbitsdmp.app.utils;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.parse.ParsePushBroadcastReceiver;
+import com.racecondition.ribbitsdmp.app.ui.MainActivity;
+
+/**
+ * Created by tboland on 8/3/15.
+ */
+public class Receiver extends ParsePushBroadcastReceiver {
+
+    @Override
+    public void onPushOpen(Context context, Intent intent) {
+        Intent i = new Intent(context, MainActivity.class);
+        i.putExtras(intent.getExtras());
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
+    }
+}
